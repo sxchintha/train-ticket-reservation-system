2b4604369170fba25a212786e20969d0fa79fadb
Sachintha Gunaratne <sachinthasl99@gmail.com>
2023-10-12 03:04:36 +0530

    start implementing my reservations

---
+++ Tickect Reservation App/app/src/main/java/com/tickectreservation/MyReservations.java
@@ -0,0 +1,14 @@
+package com.tickectreservation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MyReservations extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.my_reservations);
+    }
+}
